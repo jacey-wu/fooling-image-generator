fb6589f1333d978fc62fe3d7e0e7943d79654cc3
Jacey Wu <jiaxiao.wu@mail.utoronto.ca>
2018-10-07 22:38:18 -0400

    Added script to train a simple CNN MNIST classifier

---
+++ classifier/train_mnist_classifier.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+from time import localtime, strftime
+import os
+
+import utils.cnn_utils as cnn
+
+# mnist = tf.keras.datasets.mnist
+# (x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets(os.path.join(".", "..", "MNIST_data"), one_hot=True)
+
+
+def main(argv=None):
+
+    with tf.device('/gpu:0'):
+
+        # Input layer
+        x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
+        y = tf.placeholder(tf.float32, shape=[None, 10], name="y")
+        keep_prob = tf.placeholder(tf.float32)
+
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
+        print("Finished building inputs and outputs")
+
+        # Classifier architecture
+        with tf.variable_scope("classifier", reuse=tf.AUTO_REUSE):
+
+            # Convolutional layer 1
+            W_conv1 = cnn.weight_variable([5, 5, 1, 32], name="W_conv1")
+            b_conv1 = cnn.bias_variable([32], name="b_conv1")
+
+            h_conv1 = tf.nn.relu(cnn.conv2d_basic(x_image, W_conv1, b_conv1), name="h_conv1")
+            h_pool1 = cnn.max_pool_2x2(h_conv1)
+
+            # Convolutional layer 2
+            W_conv2 = cnn.weight_variable([5, 5, 32, 64], name="W_conv2")
+            b_conv2 = cnn.bias_variable([64], name="b_conv2")
+
+            h_conv2 = tf.nn.relu(cnn.conv2d_basic(h_pool1, W_conv2, b_conv2), name="h_conv2")
+            h_pool2 = cnn.max_pool_2x2(h_conv2)
+
+            # Fully connected layer 1
+            h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+
+            W_fc1 = cnn.weight_variable([7 * 7 * 64, 1024], name="W_fc1")
+            b_fc1 = cnn.bias_variable([1024], name="b_fc1")
+
+            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name="h_fc1")
+
+            # Dropout
+            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob, name="h_fc1_drop")
+
+            # Fully connected layer 2 (Output layer)
+            W_fc2 = cnn.weight_variable([1024, 10], name="W_fc2")
+            b_fc2 = cnn.bias_variable([10], name="b_fc2")
+
+            y_ = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y_')
+        print("Finished building MNIST classifier")
+
+        # Evaluation metrics
+        cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y_), reduction_indices=[1]), name="cross_entropy")
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+        print("Finished building metrics")
+
+        # Training algorithm
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+        print("Finished building training operation")
+
+        # Log operation
+        log_accuracy = tf.summary.scalar("accuracy", accuracy)
+
+        # GPU memory tricks, so that computation doesn't take all GPU resource at once
+        gpu_options = tf.GPUOptions(allow_growth=True)
+        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)) as sess:
+
+            # Init variables
+            sess.run(tf.global_variables_initializer())
+
+            # Set up model saving
+            model_dir = os.path.join(".", "model")
+
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            model_saver = tf.train.Saver(max_to_keep=10)
+
+            # Set up logger
+            log_dir = os.path.join(".", "log", strftime("%Y-%m-%d-%H%M%S", localtime()))
+
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            summary_writer = tf.summary.FileWriter(log_dir)
+
+            # Train classifier
+            max_epoch = 20
+            batch_size = 50
+            keep_probab = 0.5
+
+            curr_epoch = 0
+            max_step = int((mnist.train.images.shape[0] * max_epoch) / batch_size)
+            for step in range(max_step):
+
+                if (step * batch_size) % mnist.train.images.shape[0] == 0:
+
+                    # Log and report accuracy
+                    accu, summary_str = sess.run([accuracy, log_accuracy],
+                                                 feed_dict={x: mnist.validation.images,
+                                                            y: mnist.validation.labels, keep_prob: 1.0})
+                    summary_writer.add_summary(summary_str, step)
+                    print("Step = {}, epoch = {}, validation accuracy = {}".format(step, curr_epoch, accu))
+
+                    # Save model weights
+                    model_saver.save(sess, os.path.join(model_dir, "mnist_classifier.ckpt"), step)
+                    curr_epoch += 1
+
+                batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+                sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: keep_probab})
+
+            accu, summary_str = sess.run([accuracy, log_accuracy],
+                                         feed_dict={x: mnist.validation.images,
+                                                    y: mnist.validation.labels, keep_prob: 1.0})
+            summary_writer.add_summary(summary_str, max_step)
+            print("Step = {}, epoch = {}, validation accuracy = {}".format(max_step, max_epoch, accu))
+            model_saver.save(sess, os.path.join(model_dir, "mnist_classifier.ckpt"), max_step)
+
+
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ utils/cnn_utils.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+
+
+def weight_variable(shape, stddev=0.02, name=None):
+    initial = tf.truncated_normal(shape, stddev=stddev)
+    if name is None:
+        return tf.Variable(initial)
+    else:
+        return tf.get_variable(name, initializer=initial)
+
+
+def bias_variable(shape, name=None):
+    initial = tf.constant(0.0, shape=shape)
+    if name is None:
+        return tf.Variable(initial)
+    else:
+        return tf.get_variable(name, initializer=initial)
+
+
+def conv2d_basic(x, W, bias):
+    conv = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding="SAME")
+    return tf.nn.bias_add(conv, bias)
+
+
+def batch_norm(x, n_out, phase_train, scope='bn', decay=0.9, eps=1e-5):
+    """
+        Code taken from http://stackoverflow.com/a/34634291/2267819
+    """
+    with tf.variable_scope(scope):
+        beta = tf.get_variable(name='beta', shape=[n_out], initializer=tf.constant_initializer(0.0)
+                               , trainable=True)
+        gamma = tf.get_variable(name='gamma', shape=[n_out], initializer=tf.random_normal_initializer(1.0, 0.02),
+                                trainable=True)
+        batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+        ema = tf.train.ExponentialMovingAverage(decay=decay)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(phase_train,
+                            mean_var_with_update,
+                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, eps)
+    return normed
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
